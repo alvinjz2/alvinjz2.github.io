615d2542009ed8e072c18fe9e6e1911bef6cf53b
Alvin Zhang <alvin.zhang295@gmail.com>
2022-08-11 01:09:07 -0700

    Update card.js
    Fix resizing issues

--- mysite/src/components/card.js
+++ mysite/src/components/card.js
@@ -11,9 +11,9 @@ function Card(props) {
       class="bg-slate-100 drop-shadow-lg rounded-xl min-w-fit max-w-lg p-6 transition ease-in-out duration-300 hover:scale-105"
       onClick={routeChange}
     >
-      <div class="place-items-center">
-        <div class="grid grid-rows-2 ">
-          <p class="text-4xl pb-6 font-medium">{props.title} </p>
+      <div class="">
+        <div class="grid grid-rows-2 place-items-center">
+          <p class="text-4xl p-2 font-medium">{props.title} </p>
           <p class="text-xl">{props.content}</p>
         </div>
       </div>
