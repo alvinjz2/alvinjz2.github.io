c1637df57f88687e2c93964c46777d5b0d187468
Alvin Zhang <alvin.zhang295@gmail.com>
2022-08-11 01:10:58 -0700

    Update about.js
    Finish most of about me page, added content, header, and spacing

--- mysite/src/pages/about.js
+++ mysite/src/pages/about.js
@@ -1,13 +1,37 @@
 import React from "react";
 import Back from "../components/back";
+import TextCard from "../components/text";
+import Header from "../components/header";
+import PortraitPhoto from "../components/portraitphoto";
+import GitHubIcon from "../components/githublink";
+import LinkedInIcon from "../components/linkedinlink";
+import TwitterIcon from "../components/twitterlink";
 
 export default function About() {
   return (
-    <div>
-      <header className="App-header">
-        <p>about</p>
-        <Back />
-      </header>
+    <div class="bg-gradient-to-r from-transparent via-fuchsia-200 to-violet-100 h-screen w-full content-center">
+      <div class="grid grid-cols-2">
+        <div class="grid place-content-around translate-y-[10%]">
+          <Header title="Hello, I'm Alvin!" />
+          <TextCard
+            content="Currently, I'm a student at the University of Illinois Urbana Champaign.
+                             I study Computer Engineering, and am set to graduate in the Spring of 2024."
+          />
+          <TextCard
+            content="At the moment, I'm a member of Quant, a RSO at my school that focuses on Quantitative Trading. I 
+                      also act as a robotics mentor for the FIRST Tech Challenge team I founded a few years ago, Future."
+          />
+          <div class="flex flex-row gap-6">
+            <Back />
+            <GitHubIcon />
+            <LinkedInIcon />
+            <TwitterIcon />
+          </div>
+        </div>
+        <div class="translate-y-[13%]">
+          <PortraitPhoto />
+        </div>
+      </div>
     </div>
   );
 }
