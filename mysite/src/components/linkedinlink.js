b2576b50c614eb52ca95e7330b04a97716c2bdb3
Alvin Zhang <alvin.zhang295@gmail.com>
2022-08-11 15:57:32 -0700

    Update linkedinlink.js
    Finish this component

--- mysite/src/components/linkedinlink.js
+++ mysite/src/components/linkedinlink.js
@@ -1,14 +1,17 @@
 import React from "react";
 const linkedin_link = "https://www.linkedin.com/in/alvinjz2/";
-const image_path = require("./media/GitHubIcon.png");
+const image_path = require("./media/LinkedInIcon.png");
 
 export default function LinkedInIcon() {
   return (
-    <div class="pb-10 pt-8 place-items-center">
-      <a href={linkedin_link}>
-        <button class="text-xl w-14 h-[52px] bg-zinc-200 py-3 px-6 drop-shadow-lg rounded-full hover:bg-indigo-100 ease-in-out transition duration-500">
-          {/* <img class="" src={image_path} alt="GitHub Profile"></img> */}
-          LinkedIn
+    <div class="pb-10 pt-8 translate-y-1">
+      <a rel="noopener noreferrer" target="_blank" href={linkedin_link}>
+        <button class="flex overflow-visible bg-white shadow-lg rounded-md hover:scale-110 ease-in-out transition duration-300">
+          <img
+            class=" w-[46px] h-[46px]"
+            src={image_path}
+            alt="LinkedIn Profile"
+          ></img>
         </button>
       </a>
     </div>
