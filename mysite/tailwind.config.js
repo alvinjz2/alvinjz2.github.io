fc9c3a99c426f910ae191623d5d70121010dece4
Alvin Zhang <alvin.zhang295@gmail.com>
2022-08-11 01:10:31 -0700

    Update tailwind.config.js
    add jit mode and purge for custom values

--- mysite/tailwind.config.js
+++ mysite/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/*.{html,js}", "./src/components/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
 };
