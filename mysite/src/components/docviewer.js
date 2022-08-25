feb79b5a96be7bb1ed0ed4aeed9531de995a3cc9
Alvin Zhang <alvin.zhang295@gmail.com>
2022-08-25 11:48:50 -0500

    Links styling
    Update height

--- mysite/src/components/docviewer.js
+++ mysite/src/components/docviewer.js
@@ -2,7 +2,7 @@ import React from "react";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 export default function DView() {
-  const docs = [{ uri: require("./media/Resume.pdf") }];
+  const docs = [{ uri: require("./media/AlvinZhangResume.pdf") }];
   return (
     <div class="w-5/6 pb-0">
       <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
--- mysite/src/components/links/linkedinlink.js
+++ mysite/src/components/links/linkedinlink.js
@@ -3,11 +3,11 @@ import LinkedInImage from "../media/LinkedInIcon.png";
 
 export default function LinkedInIcon(props) {
   return (
-    <div class="pt-8 translate-y-[5px]">
+    <div class="pt-8 translate-y-[3px]">
       <a rel="noopener noreferrer" target="_blank" href={props.link}>
         <button class="flex overflow-visible bg-white shadow-md rounded-md hover:scale-110 ease-in-out transition duration-300">
           <img
-            class=" w-[48px] h-[48px]"
+            class=" w-[50px] h-[50px]"
             src={LinkedInImage}
             alt="LinkedIn Profile"
           ></img>
