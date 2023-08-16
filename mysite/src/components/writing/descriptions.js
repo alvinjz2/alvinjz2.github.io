366c12fd1ab304a5c72908fe5b410abff4119734
Alvin Zhang <alvin.zhang295@gmail.com>
2023-08-15 17:45:03 -0700

    move description out to its own file
    declutter the page source code

---
+++ mysite/src/components/writing/descriptions.js
@@ -0,0 +1,7 @@
+var bio =
+  "I am a student at the University of Illinois Urbana Champaign, majoring in Statistics and Computer Science. Apart from my schoolwork, I also am an Undergraduate Research Assistant at the TIMAN Group. ";
+var secondary_bio =
+  "I also like to play Chess, along with a variety of different PC/Mobile games.";
+
+export { bio };
+export { secondary_bio };
