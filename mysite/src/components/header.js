137b4da138af570b8ec944235e403305d29663aa
Alvin Zhang <alvin.zhang295@gmail.com>
2023-08-15 17:58:02 -0700

    add styling

--- mysite/src/components/header.js
+++ mysite/src/components/header.js
@@ -3,14 +3,19 @@ import React from "react";
 function Header(props) {
   return (
     <div class="py-15">
-    <div class="grid bg-white drop-shadow-md rounded-xl max-w-lg place-items-center">
-    <p class="p-10 text-7xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-violet-300 via-violet-200 to-sky-300">
-      {props.title}
-    </p>
-
+      <div
+        class="grid bg-white drop-shadow-md rounded-xl max-w-lg"
+        style={props.mstyle}
+      >
+        <p
+          class="text-7xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-violet-300 via-violet-200 to-sky-300"
+          style={props.hstyle}
+        >
+          {props.title}
+        </p>
+        <p style={props.sstyle}>{props.subtitle}</p>
+      </div>
     </div>
-  </div>
-
   );
 }
 
