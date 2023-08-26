89e4de70717302c301f2ac4fdc961cd44c594621
Alvin Zhang <alvin.zhang295@gmail.com>
2023-08-26 10:39:46 -0500

    Update descriptions.js

--- mysite/src/components/writing/descriptions.js
+++ mysite/src/components/writing/descriptions.js
@@ -1,5 +1,5 @@
 var bio =
-  "I am a student at the University of Illinois Urbana Champaign, majoring in Statistics and Computer Science.";
+  "I am a student at the University of Illinois Urbana Champaign, majoring in Statistics and Computer Science. I've had the pleasure of interning at Capital One and VertexGraph, and am looking for full time positions in Software Engineering!";
 var secondary_bio =
   "I also like to play Chess, along with a variety of different PC/Mobile games.";
 
