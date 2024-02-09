52bf8ddb1c3664b61ed06ceba7c073b25f7765a8
Alvin Zhang <alvin.zhang295@gmail.com>
2024-02-08 23:08:51 -0800

    Create discordlink.js
    create a button for discord

---
+++ mysite/src/components/links/discordlink.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GitHubImage from "../media/DiscordIcon.png";
+
+let discordLink = 'https://discord.gg/mQCJvGbFTm'
+
+export default function DiscordIcon(props) {
+  return (
+    <div class="place-items-center">
+      <a rel="noopener noreferrer" target="_blank" href={discordLink}>
+        <button class="flex overflow-hidden h-[54px] bg-zinc-200 shadow-md rounded-full hover:scale-110 ease-in-out transition duration-300">
+          <img class="w-[54px] h-[54px]" src={DiscordImage} alt="Discord"></img>
+        </button>
+      </a>
+    </div>
+  );
+}
