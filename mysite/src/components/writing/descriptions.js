de45ada24b5a2d03c8e8863eeb8af9a84eb0af14
Alvin Zhang <alvin.zhang295@gmail.com>
2024-02-10 18:40:45 -0800

    Delete mysite/src/components/writing directory
    unnecessary directory and files

--- mysite/src/components/writing/descriptions.js
+++
@@ -1,7 +0,0 @@
-var bio =
-  "I am a student at the University of Illinois Urbana Champaign, majoring in Statistics and Computer Science. I've had the pleasure of interning at Capital One and VertexGraph, and am looking for full time positions in Software Engineering!";
-var secondary_bio =
-  "I also like to play Chess, along with a variety of different PC/Mobile games.";
-
-export { bio };
-export { secondary_bio };
