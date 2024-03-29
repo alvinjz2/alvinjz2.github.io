0669f41f0d980f56b27c59fdebacc8984dbf549d
Alvin Zhang <alvin.zhang295@gmail.com>
2024-03-28 23:22:22 -0700

    add gif next to subtitle

--- mysite/src/components/header.js
+++ mysite/src/components/header.js
@@ -1,16 +1,23 @@
 import React from "react";
-
+import Gif from "./media/parrot.gif"
 function Header(props) {
   return (
     <div class="py-15">
+      
       <div>
         <p
-          class="text-7xl font-medium mb-4"
+          class="text-7xl font-medium mb-5"
           style={props.hstyle}
         >
           {props.title}
         </p>
-        <p class="text-3xl font-medium" style={props.sstyle}>{props.subtitle}</p>
+        <div className="flex items-center">
+        <p class="text-4xl font-small">{props.subtitle}</p>
+        <img src={Gif} alt="parrot" class=" ml-4 h-10"/>
+        </div>
+        
+        
+        <p class="text-3xl font-small mt-4">{props.email}</p>
       </div>
     </div>
   );
