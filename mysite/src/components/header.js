ea4d900c3f40f8cba55f614881a1fb58b719d2ec
Alvin Zhang <alvin.zhang295@gmail.com>
2024-04-16 20:06:12 -0700

    Update header.js

--- mysite/src/components/header.js
+++ mysite/src/components/header.js
@@ -1,23 +1,18 @@
 import React from "react";
 import Gif from "./media/parrot.gif"
+
 function Header(props) {
   return (
-    <div class="py-15">
-      
+    <div className="py-15">
       <div>
-        <p
-          class="text-7xl font-medium mb-5"
-          style={props.hstyle}
-        >
-          {props.title}
-        </p>
+      <p className="text-5xl sm:text-4xl md:text-7xl lg:text-7xl xl:text-7xl font-medium mb-5" style={props.hstyle}>
+        {props.title}
+      </p>
         <div className="flex items-center">
-        <p class="text-4xl font-small">{props.subtitle}</p>
-        <img src={Gif} alt="parrot" class=" ml-4 h-10"/>
+          <p className="text-3xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl font-light mt-1">{props.subtitle}</p>
+          <img src={Gif} alt="parrot" className="ml-4 h-10" />
         </div>
-        
-        
-        <p class="text-3xl font-small mt-4">{props.email}</p>
+        <p className="text-2xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-3xl font-light mt-5 ">{props.email}</p>
       </div>
     </div>
   );
